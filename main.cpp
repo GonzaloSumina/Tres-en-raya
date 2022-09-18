196ce410014536b4429dcccff0976a0549cc53a4
GonzaloSumina <113203761+GonzaloSumina@users.noreply.github.com>
2022-09-17 19:55:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
-//#include <cmath>
+#include <string>
 
 int main() {
-    //std::system("cls");
+    std::system("cls");
     std::cout << "Bienvenidos al juego Tres en Raya\nUCSP-Ciencia de la Computacion 2-1";
     std::cout << "Nombres.\n Gonzalo Sumina\n Juan Palo\n";
-    char a=' ', b=' ', c=' ', d=' ', e=' ', f=' ', g=' ', h=' ', i=' ';
+    char a='1', b='2', c='3', d='4', e='5', f='6', g='7', h='8', i='9';
+    std::string fila="\033[31m----------\033[m";
+    std::string columna="\033[31m | \033[m";
     //color rojo : "\033["<<31<<"m"<<valor a colorear<<"\033[m"
     //color azul : "\033["<<34<<"m"<<valor a colorear<<"\033[m"
     char variable;
     char ficha1, ficha2;
-    std::cout << a << " | " << b << " | " << c << std::endl;
-    std::cout << "----------"<<std::endl;
-    std::cout << d << " | " << e << " | " << f << std::endl;
-    std::cout << "----------"<<std::endl;
-    std::cout << g << " | " << h << " | " << i << std::endl;
+    std::cout << a << columna << b << columna << c << std::endl;
+    std::cout << fila<<std::endl;
+    std::cout << d << columna << e << columna << f << std::endl;
+    std::cout << fila<<std::endl;
+    std::cout << g << columna << h << columna << i << std::endl;
 
     std::cout << "Ingrese el simbolo que representara al Jugador1: ";
     std::cin >> ficha1;
@@ -122,11 +124,24 @@ int main() {
             x=9;
             break;
         }
-        std::cout << a << " | " << b << " | " << c << std::endl;
-        std::cout << "----------"<<std::endl;
-        std::cout << d << " | " << e << " | " << f << std::endl;
-        std::cout << "----------"<<std::endl;
-        std::cout << g << " | " << h << " | " << i << std::endl;
+        std::system("cls");
+        std::cout << a << columna << b << columna << c << std::endl;
+        std::cout << fila<<std::endl;
+        std::cout << d << columna << e << columna << f << std::endl;
+        std::cout << fila<<std::endl;
+        std::cout << g << columna << h << columna << i << std::endl;
+        if (x>=4){
+           if ((a==b && b==c) || (a==d && d==g) || (a==e && e==i)||
+              (b==e && e==h) || (c==f && f==i) || (c==e && e==g) ||
+              (d==e && e==f) || (g==h && h==i)){
+              std::cout << "Felicidades! Gano el Jugador "<<ficha1;
+              return 0;
+           }
+           else if (x==9){
+              std::cout << "Empate";
+              return 0;
+           }
+        }
       }
       if ((x+1)%2==0){
         switch(variable)
@@ -226,13 +241,27 @@ int main() {
             x=9;
             break;
         }
-        std::cout << a << " | " << b << " | " << c << std::endl;
-        std::cout << "----------"<<std::endl;
-        std::cout << d << " | " << e << " | " << f << std::endl;
-        std::cout << "----------"<<std::endl;
-        std::cout << g << " | " << h << " | " << i << std::endl;
+        std::system("cls");
+        std::cout << a << columna << b << columna << c << std::endl;
+        std::cout << fila<<std::endl;
+        std::cout << d << columna << e << columna << f << std::endl;
+        std::cout << fila<<std::endl;
+        std::cout << g << columna << h << columna << i << std::endl;
+        if (x>=4){
+          if ((a==b && b==c) || (a==d && d==g) || (a==e && e==i)||
+              (b==e && e==h) || (c==f && f==i) || (c==e && e==g) ||
+              (d==e && e==f) || (g==h && h==i)){
+            std::cout << "Felicidades! Gano el Jugador "<<ficha2;
+            return 0;
+          }
+          else if (x==9){
+            std::cout << "Empate";
+            return 0;
+          }
+        }
       }
     }
+    
 
     return 0;
 
